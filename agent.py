c4df0dbe8f80cf3c747b8b3edc9a800703493d89
smithazura <smithazura11@gmail.com>
2025-03-23 09:25:48 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,1 @@
-Real programmers don’t comment—if it was hard to write, it should be hard to understand.The best code is self-explanatory.Code never lies, comments sometimes do.Hello, world!ag
+Good documentation saves lives (or at least hours of frustration).Real programmers don’t comment—if it was hard to write, it should be hard to understand.The best code is self-explanatory.Code never lies, comments sometimes do.Hello, world!ag
